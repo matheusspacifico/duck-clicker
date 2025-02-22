9d6f3d821835080c137f1f746e856471acb8eca1
matheusspacifico <matheuspacifico44@gmail.com>
2025-02-21 21:51:36 -0300

    feat: duck squishing and pickaxe mining animations

--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ const duck = document.getElementById("duck-protagonist");
 const pickaxe = document.getElementById("pickaxe");
 
 function duckClick() {
-    duck.classList.add("squish-animation");
-    pickaxe.classList.add("mine-animation");
 
-    setTimeout(() => {
-        duck.classList.remove("squish-animation");
-        pickaxe.classList.remove("mine-animation");
-    }, 100);
 }
 
 clickArea.addEventListener("click", duckClick);
